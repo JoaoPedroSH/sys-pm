cea8b986800f492ae62435f49527646d868bb4a0
JoaoPedroSH <joaopedro.shalom27@gmail.com>
2022-09-23 03:07:26 -0300

    UP

--- SysPM/services/EditandoUsuario.php
+++ SysPM/services/EditandoUsuario.php
@@ -10,13 +10,13 @@ $id = $_POST['id'];
 if (isset($user)) {
 
   $query = "UPDATE `usuario` SET  `user`= '$user',`senha`='$senha',`tipo_user`='$acesso' WHERE id_usuario='$id'";
-  echo $query;
+
   $result = mysqli_query($conexao, $query);
 
   if ($result == 1) {
-    $_SESSION['edit_permissao'] = true;
+    $_SESSION['success_edit'] = true;
     header('Location: ../pages/adm/permissao.php');
   } else {
-    $_SESSION['nao_editado'] = true;
+    $_SESSION['error_edit'] = true;
   }
 }
